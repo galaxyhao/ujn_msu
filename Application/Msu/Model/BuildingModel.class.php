5ca8c2a362b2131d284d78ff7260212af43bd657
galaxyhao <GalaxyHao214@gmail.com>
2017-09-18 02:16:14 +0800

    增加楼号新增验证 关闭调试模式

--- Application/Msu/Model/BuildingModel.class.php
+++ Application/Msu/Model/BuildingModel.class.php
@@ -6,6 +6,7 @@ class BuildingModel extends Model
 	protected $_validate = array(
 		array('name','require','楼号不能为空',0,'require',1),
 		array('type',array('职工楼','办公楼'),'类型输入不正确',0,'in'),
+		array('name','','该楼号已存在',0,'unique')
 		);
 }
 ?>
